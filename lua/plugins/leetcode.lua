4018a07e5961946b44b8cf48b9c0916f439f0f0c
huncholane <huncho@hygo.ai>
2025-04-18 22:01:00 -0700

    -- fix leetcode keys

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,15 +1,12 @@
 return {
   "kawre/leetcode.nvim",
-  event="VeryLazy",
+  event = "VeryLazy",
   dependencies = {
     "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
   },
   keys = {
-    { "<leader>z",function ()
-      vim.cmd("Leet")
-    end, desc = "LeetCode" },
     {
       "<leader>zt",
       function()
@@ -24,46 +21,14 @@ return {
       end,
       desc = "Submit",
     },
-    {
-      "<leader>zh",
-      function()
-        vim.cmd("Leet")
-      end,
-      desc = "Home Page",
-    },
-    {
-      "<leader>zl",
-      function()
-        vim.cmd("Leet")
-        vim.cmd("sleep 10m")
-        vim.cmd("Leet list")
-      end,
-      desc = "Problem List",
-    },
-    {
-      "<leader>zr",
-      function()
-        vim.cmd("Leet random")
-      end,
-      desc = "Random Question",
-    },
-    {
-      "<leader>zo",
-      function()
-        vim.cmd("Leet open")
-      end,
-      desc = "Open in Browser",
-    },
-    {
-      "<leader>zd",
-      function()
-        vim.cmd("Leet daily")
-      end,
-      desc = "Daily Question",
-    },
-
   },
   opts = {
     lang = "golang",
   },
+  config = function(_, opts)
+    require("which-key").add({
+      { "<leader>z", group = "LeetCode" },
+    })
+    require("leetcode").setup(opts)
+  end,
 }
