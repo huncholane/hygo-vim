a7dfb47f1c8e0a2dff8a98709ac7affa19f4898d
huncholane <huncho@hygo.ai>
2025-04-20 04:10:01 -0700

    added auto-session and fixed neotree for wsl

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,10 +1,22 @@
 return {
   {
+    -- https://github.com/nvim-neo-tree/neo-tree.nvim/wiki/Recipes#open-with-system-viewer
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
-      hijack_netrw_behavior = "disabled",
-      follow_current_file = {
-        enabled = true,
+      filesystem = {
+        hijack_netrw_behavior = "disabled",
+        window = {
+          mappings = {
+            ["O"] = "system_open",
+          },
+        },
+      },
+      commands = {
+        system_open = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          vim.fn.jobstart({ "wslview", path }, { detach = true })
+        end,
       },
     },
   },
