6c32118cc36d20b877cfdd8944c2675d0a0e5f7b
huncholane <huncho@hygo.ai>
2025-04-30 17:21:39 -0700

    added dotenv and set lsp to basedpyright for python

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,8 +2,23 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      diagnostics = {
+        underline = true,
+      },
       servers = {
+        basedpyright = {
+          enabled = true,
+          settings = {
+            basedpyright = {
+              analysis = {},
+            },
+            python = {
+              pythonPath = "/home/huncho/anaconda3/bin/python",
+            },
+          },
+        },
         pyright = {
+          enabled = false,
           settings = {
             python = {
               pythonPath = "/home/huncho/anaconda3/bin/python",
@@ -21,5 +36,18 @@ return {
         enabled = false,
       },
     },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      vim.list_extend(opts.sources, {
+        -- nls.builtins.diagnostics.mypy.with({
+        --   command = "mypy",
+        --   extra_args = { "--config-file", vim.fn.getcwd() .. "/mypy.ini" },
+        -- }),
+      })
+    end,
   },
 }
